08e31fcd7ce242c47f8c6ef01547a40f9e66984c
Liam <mr.liumingjie@gmail.com>
2022-02-19 21:47:23 +0800

    test commit

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include "list.h"
+#include <iostream>
+#include <initializer_list>
+#include <utility>
+#include <array>
+
+using namespace MYSTL;
+using std::cout;
+using std::endl;
+
+
+int main() {
+    std::array a;
+    list<int> ilist1, ilist2, ilist3;
+    list<int> tmp(ilist3);
+    for (int i = 0; i < 10; i ++)
+        ilist1.push_back(i * 4);
+    
+    tmp = ilist1;
+    list<int> a(tmp.begin(), tmp.end());
+    cout << a.size() << endl;
+    
+    for(auto i:a)
+        cout << i << endl;
+
+    a = {3, 4, 5};
+    cout << a.size() << endl;
+    list<double> b{2.3, 3.3, 2};
+    b.assign({3, 3.3, 44, 4.44, 44, 3.3});
+
+    for(auto i:b)
+        cout << i << endl;
+
+    std::pair<int, int> a;
+    return 0;
+}
